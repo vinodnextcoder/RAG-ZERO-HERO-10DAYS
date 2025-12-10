bfb7a689c967ca327b1870d4f9055c4cd925c81d
vinod <vinodk2757@gmail.com>
2025-12-10 10:14:29 +0530

    read files and directory improve code

--- Day-01_Python_Foundations_for_GenAI/Task5_File_Batch_Processor/main.py
+++ Day-01_Python_Foundations_for_GenAI/Task5_File_Batch_Processor/main.py
@@ -7,20 +7,33 @@ class DocumentManager:
         self.files = os.listdir(file_path)
         
     def load_document(self):
-        print('------------File Reading-----------------')
-        print("Loading document...")
-        print(f"Directory path: {self.files}")
-        try:
-            for file_name in self.files:
-                file_name = os.path.join(self.file_path, file_name)    
-                with open(file_name, 'r', encoding='utf-8') as file:
+        directory_path = self.file_path
+        if not os.path.exists(directory_path):
+            print(f"Directory not found: {directory_path}")
+            return
+
+        if not os.path.isdir(directory_path):
+            print(f"Not a directory: {directory_path}")
+            return
+        for filename in os.listdir(directory_path):
+            try:
+                full_path = os.path.join(directory_path, filename)
+                if not os.path.exists(full_path):
+                    print(f"Not found: {full_path}")
+                    continue
+                
+                print(f"Processing: {full_path}")
+                with open(full_path, 'r', encoding='utf-8') as file:
                     self.content = file.read()
                     print("Document Content:")
                     print(self.content)
-        except FileNotFoundError:
-            print(f"The file at {self.files} does not exist.")
-        except Exception as e:
-            print(f"An error occurred: {e}")
+            except FileNotFoundError:
+                print(f"The file at {self.files} does not exist.")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+
+            
+
 
 
 doc = DocumentManager("./data/")
