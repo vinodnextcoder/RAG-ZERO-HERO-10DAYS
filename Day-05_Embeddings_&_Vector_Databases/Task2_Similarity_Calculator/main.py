b5868fcb0426c9730945a483ddef2f3f891f9be2
vinod <vinodk2757@gmail.com>
2025-12-14 22:42:20 +0530

    cosine simalirity

---
+++ Day-05_Embeddings_&_Vector_Databases/Task2_Similarity_Calculator/main.py
@@ -0,0 +1,23 @@
+import os
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+from sentence_transformers import SentenceTransformer
+
+def similarity_calculator():
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+    text1 = "dog"
+    text2 = "puppy"
+
+    query_embedding = model.encode(text1)  # 2D
+    doc_embeddings = model.encode(text2)  # 2D
+
+    score = cosine_similarity([query_embedding], [doc_embeddings])[0][0]
+    print("Cosine similarity:", score)
+    cos_sim = np.dot(query_embedding, doc_embeddings) / (np.linalg.norm(query_embedding) * np.linalg.norm(doc_embeddings))
+
+    print("Cosine similarity:", cos_sim)
+
+
+
+
+similarity_calculator()
