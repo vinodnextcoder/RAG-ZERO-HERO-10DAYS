2e8be00ed27208326d5bb2cf7f17509cd0b927dd
vinod <vinodk2757@gmail.com>
2025-12-15 11:03:28 +0530

    chromadb get database

--- Day-05_Embeddings_&_Vector_Databases/Task3_ChromaDB_Document_Store/main.py
+++ Day-05_Embeddings_&_Vector_Databases/Task3_ChromaDB_Document_Store/main.py
@@ -1,32 +1,8 @@
-import os,re,sys
-from pypdf import PdfReader
+
 from dotenv import load_dotenv
-from openai import OpenAI
-from sentence_transformers import SentenceTransformer
-import numpy as np
 import chromadb
 load_dotenv()
 
-
-# 1️⃣ Read API key
-api_key = os.getenv("OPENROUTER_API_KEY")
-
-# 2️⃣ Handle missing key
-if not api_key:
-    print("❌ ERROR: OPENROUTER_API_KEY not found.")
-    print("👉 Please set it as an environment variable or in a .env file.")
-    print("👉 Example:")
-    print("   export OPENROUTER_API_KEY='your_api_key_here'  (Linux/macOS)")
-    print("   setx OPENROUTER_API_KEY \"your_api_key_here\"   (Windows)")
-    sys.exit(1)
-
-# client = OpenAI(
-#   base_url="https://openrouter.ai/api/v1",
-#   api_key=api_key
-# )
-
-
-load_dotenv()
 class ChromaDBDocument:
     def __init__ (self,collection_name):
         self.collection = collection_name
@@ -68,14 +44,14 @@ class ChromaDBDocument:
             n_results=2
         )
 
+        print("****************search******************")
         print("Similar documents:")
         for i, doc in enumerate(results['documents'][0]):
             print(f"{i+1}. {doc}")
             print(f"   Metadata: {results['metadatas'][0][i]}")
-        results1 = self.collection.get(ids=["doc1"])
 
     def get_by_id(self):
-
+        print("****************search by id******************")
         results = self.collection.get(ids=["doc1","doc2"])
         for doc_id, doc_text, metadata in zip(
             results["ids"], results["documents"], results["metadatas"]
@@ -85,8 +61,16 @@ class ChromaDBDocument:
             print("Metadata:", metadata)
             print("-" * 40)
 
+    def get_stats(self):
+        """Get collection statistics"""
+        print("*******collection state************")
+        count = self.collection.count()
+        print("total_documents",count)
+        return {"total_documents": count}
 
-readPdf = ChromaDBDocument("chromadoclearn")
-readPdf.add_context()
-readPdf.search()
-readPdf.get_by_id()
+
+read_collection = ChromaDBDocument("chromadoclearn")
+read_collection.add_context()
+read_collection.search()
+read_collection.get_by_id()
+read_collection.get_stats()
